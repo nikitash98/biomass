097a6bb604a4137bba3c1e0bbb701cbe213958e3
Nikita Shtarkman <nikitash98@gmail.com>
2023-03-14 23:19:39 -0400

    config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import ThreePointViz from './ThreePointViz';
+import React from 'react';
+function sayHello() {
+  alert('You clicked me!');
+}
+/*
+
+
+*/
 
 function App() {
+  const [playing, setPlaying] = React.useState(true);
+  const [counter, setCounter] = React.useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div class = "game_container">
+        <div className="vis-container">
+          <ThreePointViz playing = {playing} counter = {counter}/>
+        </div>
+      </div>
+      <button type="button" onClick={()=>setCounter(counter - 1)}>RIGHT!</button>
+
+      <button type="button" onClick={()=>setCounter(counter + 1)}>LEFT!</button>
+
     </div>
   );
 }
---
+++ src/Model.js
@@ -0,0 +1,75 @@
+import { useLoader, useFrame } from 'react-three-fiber';
+import { 
+    GLTFLoader 
+} from 'three/examples/jsm/loaders/GLTFLoader';
+import { useRef, useState } from 'react'
+
+import * as THREE from 'three'
+const afunction = () => {
+    console.log("HERE")
+}
+const Model = props => {
+    const model = useLoader(
+        GLTFLoader,
+        props.path
+    )
+    
+    const handleHover = (e) => {
+        e.stopPropagation()
+        setHovered(true)
+        console.log(e)
+        ec
+    }
+      
+      const handleUnhover = () => {
+        setHovered(false)
+    }
+
+
+    const [hovered, setHovered] = useState(false)
+
+    // Here's the animation part
+    // ************************* 
+    let mixer
+    if (model.animations.length) {
+        mixer = new THREE.AnimationMixer(model.scene);
+        model.animations.forEach(clip => {
+            const action = mixer.clipAction(clip)
+            action.play();
+        });
+    }
+    console.log(props.playing)
+
+    useFrame((state, delta) => {
+
+        if(props.playing) {
+            mixer?.update(delta)
+        }
+    })
+    // *************************
+
+    model.scene.traverse(child => {
+        if (child.isMesh) {
+            child.castShadow = true
+            child.receiveShadow = true
+            child.material.side = THREE.FrontSide
+            child.onPointerOver = {afunction}
+            child.onPointerOut = {handleUnhover}
+        }
+    })
+
+
+
+    return (
+        <primitive 
+            object={model.scene}
+            scale={props.scale}
+            onPointerOver = {handleHover}
+            onPointerOut = {handleUnhover}
+
+             />
+    )
+}
+
+
+export default Model;
