a6bd96eec6d61d30a65d33390173e53a4238ad10
Nikita Shtarkman <nikitashtarkman@Nikitas-MacBook-Pro.local>
2024-12-26 12:55:08 -0500

    slight fix

--- src/Items/Part01.js
+++ src/Items/Part01.js
@@ -18,7 +18,7 @@ import './3d.css'
 export function Part01(props) {
   const group = useRef()
   //const { nodes, materials, animations } = useGLTF('https://corsproxy.io/?url=https://menard.pha.jhu.edu/Biocubes/Part01.glb')
-  const { nodes, materials, animations } = useGLTF('/api/Part01.glb')
+  const { nodes, materials, animations } = useGLTF('https://biomasshosting.pages.dev/Part01.glb')
 
   const { actions } = useAnimations(animations, group)
   const imageRef_one = useRef()
